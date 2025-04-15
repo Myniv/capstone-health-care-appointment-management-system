9e6c3ef3a33339b0d4be5d42775de3c5e8807565
Mulyana N <mulyanan.ur26@gmail.com>
2025-04-15 13:56:03 +0700

    Update header and sidebar

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -4,11 +4,12 @@
     </div>
     <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="<?= base_url('admin/users'); ?>">User</a></li>
-            <li><a href="<?= base_url('admin/doctor-category'); ?>">Doctor Category</a></li>
-            <li><a href="<?= base_url('admin/doctor-schedule'); ?>">Doctor Schedule</a></li>
-            <li><a href="<?= base_url('doctor/absent'); ?>">Doctor Absent</a></li>
+            <li><a href="#" class="btn btn-white text-black mr-2">Dashboard</a></li>
+            <!-- <li><a href="<?= base_url('admin/users'); ?>">User</a></li> -->
+            <!-- <li><a href="<?= base_url('admin/doctor-category'); ?>">Doctor Category</a></li> -->
+            <!-- <li><a href="<?= base_url('admin/doctor-schedule'); ?>">Doctor Schedule</a></li> -->
+            <!-- <li><a href="<?= base_url('doctor/absent'); ?>">Doctor Absent</a></li> -->
+            <li><a href="<?= base_url('logout'); ?>" class="btn btn-error text-white">Logout</a></li>
         </ul>
     </div>
 </header>
