9b9061bd604d316f44dd2ab0fdeb35a81f78ef31
kuntowck <kunto312002@gmail.com>
2025-04-24 20:11:59 +0700

    Add upcoming appointments to doctor dashboard

--- app/Controllers/DoctorController.php
+++ app/Controllers/DoctorController.php
@@ -4,21 +4,20 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Libraries\DataParams;
+use App\Models\AppointmentModel;
 use App\Models\DoctorAbsentModel;
 use App\Models\DoctorModel;
 use CodeIgniter\HTTP\ResponseInterface;
-use CodeIgniter\I18n\Time;
-use Config\Roles;
 
 class DoctorController extends BaseController
 {
-    protected $doctorAbsentModel;
-    protected $doctorModel;
+    protected $doctorAbsentModel, $doctorModel, $appointmentModel;
 
     public function __construct()
     {
         $this->doctorAbsentModel = new DoctorAbsentModel();
         $this->doctorModel = new DoctorModel();
+        $this->appointmentModel = new AppointmentModel();
     }
 
     public function index()
@@ -28,8 +27,13 @@ class DoctorController extends BaseController
 
     public function dashboard()
     {
+        $doctorId = $this->doctorModel->where('user_id', user_id())->first()->id;
+
+        $result = $this->appointmentModel->getUpcomingAppointmentDoctor($doctorId);
+
         $data = [
-            'title' => 'Dashboard Doctor'
+            'title' => 'Dashboard Doctor',
+            'appointments' => $result
         ];
 
         return view('page/user/v_user_dashboard_doctor', $data);
--- app/Views/page/User/v_user_dashboard_doctor.php
+++ app/Views/page/User/v_user_dashboard_doctor.php
@@ -2,30 +2,62 @@
 
 <?= $this->section('content'); ?>
 <div class="container mx-auto mt-4 px-4">
-    <h2 class="text-2xl font-bold mb-4">
-        <?= $title; ?>
-    </h2>
+    <div class="grid md:grid-cols-3 gap-4">
+        <?php if (!empty(user())): ?>
+            <div class="card col-span-2">
+                <div class="card-body">
+                    <span class="card-title text-3xl font-bold">
+                        Hello, <span class="text-primary"><?= user()->username; ?></span> 👋🏼
+                    </span>
+                </div>
+            </div>
+        <?php endif; ?>
 
-    <div class="grid grid-cols-3 grid-rows-1 gap-4">
-        <div class="stats border">
+        <div class="stats border bg-secondary-content">
             <div class="stat">
-                <div class="stat-title">Total Users</div>
-                <div class="stat-value">51</div>
-                <div class="stat-desc">21% more than last month</div>
+                <div class="stat-title">Today Appointment</div>
+                <div class="stat-value text-secondary"><?= count($appointments); ?></div>
             </div>
         </div>
-        <div class="stats border">
-            <div class="stat">
-                <div class="stat-title">Total Doctors</div>
-                <div class="stat-value">23</div>
-                <div class="stat-desc">21% more than last month</div>
+
+        <div class="card border col-span-2">
+            <div class="card-body">
+                <div class="flex justify-between">
+                    <h3 class="card-title">Upcoming Appointment</h3>
+                    <a class="btn btn-sm btn-primary" href="#">View All</a>
+                </div>
+                <?php if (!empty($appointments)): ?>
+                    <ul class="list rounded-box border">
+                        <?php foreach ($appointments as $appointment): ?>
+                            <li class="list-row">
+                                <div>
+                                    <img class="size-10 rounded-full" src="<?= base_url('profile-picture?path=' . $appointment->patientProfilePicture); ?>" alt="Profile Picture <?= $appointment->patientFirstName . ' ' . $appointment->patientLastName; ?>" />
+                                </div>
+                                <div>
+                                    <div><?= $appointment->patientFirstName . ' ' . $appointment->patientLastName; ?></div>
+                                    <p>
+                                        <?= date('g:i A', strtotime($appointment->startTime)) ?> -
+                                        <?= date('g:i A', strtotime($appointment->endTime)) ?>
+                                    </p>
+                                    <p><?= date('F j, Y', strtotime($appointment->date)) ?></p>
+                                    <div class="badge badge-soft badge-warning text-xs mt-2"><?= ucwords($appointment->status); ?></div>
+                                </div>
+                                <button class="btn btn-ghost">
+                                    Manage
+                                </button>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+
+                <?php else: ?>
+                    <p>There is no data appointment.</p>
+                <?php endif; ?>
             </div>
         </div>
-        <div class="stats border">
-            <div class="stat">
-                <div class="stat-title">Total Patients</div>
-                <div class="stat-value">34</div>
-                <div class="stat-desc">21% more than last month</div>
+
+        <div class="card bg-primary-content">
+            <div class="card-body">
+                <div class="card-title">test</div>
             </div>
         </div>
     </div>
