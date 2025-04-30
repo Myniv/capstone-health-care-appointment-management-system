a9be4e4fcedf6e8bcea85740f594c3cf636a82a0
Mulyana N <mulyanan.ur26@gmail.com>
2025-04-30 21:17:10 +0700

    add pristine client validation to login page

--- app/Views/page/auth/login.php
+++ app/Views/page/auth/login.php
@@ -7,37 +7,41 @@
 
     <?= view('Myth\Auth\Views\_message_block') ?>
 
-    <form action="<?= url_to('login') ?>" method="post">
+    <form action="<?= url_to('login') ?>" method="post" id="formData">
         <?= csrf_field() ?>
 
         <!-- Login Field -->
         <?php if ($config->validFields === ['email']): ?>
-            <div class="mb-4">
+            <div class="mb-3">
                 <label for="login" class="label">
                     <span class="label-text"><?= lang('Auth.email') ?></span>
                 </label>
                 <input type="email" name="login" placeholder="<?= lang('Auth.email') ?>"
-                    class="input input-bordered w-full <?= session('errors.login') ? 'input-error' : '' ?>">
+                    class="input input-bordered w-full <?= session('errors.login') ? 'input-error' : '' ?>" required
+                    data-pristine-required-message="The email field is required.">
                 <div class="text-error text-sm mt-1"><?= session('errors.login') ?></div>
             </div>
         <?php else: ?>
-            <div class="mb-4">
+            <div class="mb-3">
                 <label for="login" class="label">
                     <span class="label-text"><?= lang('Auth.emailOrUsername') ?></span>
                 </label>
                 <input type="text" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>"
-                    class="input input-bordered w-full <?= session('errors.login') ? 'input-error' : '' ?>">
+                    class="input input-bordered w-full <?= session('errors.login') ? 'input-error' : '' ?>" required
+                    data-pristine-required-message="The email or username field is required.">
                 <div class="text-error text-sm mt-1"><?= session('errors.login') ?></div>
             </div>
         <?php endif; ?>
 
         <!-- Password Field -->
-        <div class="mb-4">
+        <div class="mb-3">
             <label for="password" class="label">
                 <span class="label-text"><?= lang('Auth.password') ?></span>
             </label>
             <input type="password" name="password" placeholder="<?= lang('Auth.password') ?>"
-                class="input input-bordered w-full <?= session('errors.password') ? 'input-error' : '' ?>">
+                class="input input-bordered w-full <?= session('errors.password') ? 'input-error' : '' ?>" required
+                data-pristine-required-message="The password field is required." data-pristine-minlength="8"
+                data-pristine-minlength-message="Password minimal 8 characters.">
             <div class="text-error text-sm mt-1"><?= session('errors.password') ?></div>
         </div>
 
@@ -71,3 +75,26 @@
 </div>
 
 <?= $this->endSection() ?>
+
+<?= $this->section('scripts') ?>
+<script>
+    window.onload = function () {
+        const form = document.getElementById("formData");
+
+        const pristine = new Pristine(form, {
+            classTo: 'mb-3',
+            errorClass: 'input-error',
+            successClass: 'input-success',
+            errorTextParent: 'mb-3',
+            errorTextTag: 'div',
+            errorTextClass: 'text-error text-sm'
+        });
+
+        form.addEventListener('submit', function (e) {
+            if (!pristine.validate()) {
+                e.preventDefault();
+            }
+        });
+    };
+</script>
+<?= $this->endSection(); ?>
