6217d64ded946a436727dc3c239a628cd64e57af
kuntowck <kunto312002@gmail.com>
2025-05-07 17:04:47 +0700

    Refactor layout files to remove unused CDN links and wrap sidebar toggle scripts in a logged-in check

--- app/Views/layouts/auth_layout.php
+++ app/Views/layouts/auth_layout.php
@@ -9,27 +9,12 @@
     <!-- Tailwind CSS & DaisyUI -->
     <link rel="stylesheet" href="<?= base_url('assets/css/tailwind.css') ?>">
 
-    <!-- Tailwind CSS via CDN -->
-    <script src="https://cdn.tailwindcss.com"></script>
-
-    <!-- DaisyUI CSS via CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@latest/dist/full.css" rel="stylesheet" />
-
-    <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Tailwind CSS & DaisyUI CDN-->
+    <!-- <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@latest/dist/full.css" rel="stylesheet" /> -->
 
     <!-- Pristine.js -->
     <script src="<?= base_url('assets/js/pristine/dist/pristine.js') ?>" type="text/javascript"></script>
-
-    <!-- jQuery UI CSS -->
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- jQuery UI -->
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
 </head>
 
 <body class="bg-base-200 min-h-screen flex flex-col" data-theme="light">
@@ -47,6 +32,5 @@
 
     <?= $this->renderSection('scripts'); ?>
 </body>
-
 
 </html>
